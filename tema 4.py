079bd5bbd1d9520f58578fab9c863782ac32c10a
Andreea24-iec <andreea.sacalis@gmail.com>
2024-11-05 15:18:36 +0200

    tema 4

---
+++ tema 4.py
@@ -0,0 +1,46 @@
+import random
+
+cuvinte = ["python", "programare", "calculator", "date", "algoritm"]
+cuvant_de_ghicit = random.choice(cuvinte)
+progres = ["_" for _ in cuvant_de_ghicit]
+
+incercari_ramase = 6
+litere_incercate = []
+
+print(" ".join(progres))
+
+
+while incercari_ramase > 0 and "_" in progres:
+
+    litera = input("Introdu o literă: ").lower()
+
+
+    if len(litera) != 1 or not litera.isalpha():
+        print("Te rog să introduci o singură literă.")
+        continue
+    if litera in litere_incercate:
+        print("Ai încercat deja litera aceasta.")
+        continue
+
+
+    litere_incercate.append(litera)
+
+
+    if litera in cuvant_de_ghicit:
+        for i, char in enumerate(cuvant_de_ghicit):
+            if char == litera:
+                progres[i] = litera
+        print("Bravo! Litera se află în cuvânt.")
+    else:
+        incercari_ramase -= 1
+        print("Litera nu se află în cuvânt. Încercări rămase:", incercari_ramase)
+
+
+    print("Progres:", " ".join(progres))
+    print("Încercări rămase:", incercari_ramase)
+
+
+if "_" not in progres:
+    print("Felicitări! Ai ghicit cuvântul:", cuvant_de_ghicit)
+else:
+    print("Ai pierdut! Cuvântul era:", cuvant_de_ghicit)
