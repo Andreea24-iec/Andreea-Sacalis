c12532911012693bd2bbc21fa321bd677a4911d6
Andreea24-iec <andreea.sacalis@gmail.com>
2024-12-12 21:24:32 +0200

    Tema 7

---
+++ Tema 7.1.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+import random
+
+
+root = tk.Tk()
+root.title("Vizualizare Bubble sort")
+canvas_width = 800
+canvas_height = 400
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="white")
+canvas.pack()
+
+n = 100
+data = [random.randint(5, 100) for _ in range(n)]
+bar_width= canvas_width//n
+
+def draw_bars(data, color="blue"):
+    canvas.delete("all")
+    for i, val in enumerate(data):
+        x0 = i * bar_width
+        y0 = canvas_height - val * 3
+        x1 = (i+1) * bar_width
+        y1 = canvas_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill=color)
+    root.update_idletasks()
+
+def bubble_sort(data):
+    n = len(data)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+                print(data)
+                draw_bars(data, color="green")
+    draw_bars(data, color="red")
+draw_bars(data)
+root.after(10, bubble_sort, data)
+
+root.mainloop()
